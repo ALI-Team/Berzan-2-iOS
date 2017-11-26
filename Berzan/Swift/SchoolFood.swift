603ea9ca22ccb9c0a9ae4a9a24e6cbeffbe782ee
Luka Jankovic <lukjan1999@gmail.com>
2017-11-26 21:58:32 +0100

    Week selection in school food

--- Berzan/Swift/SchoolFood.swift
+++ Berzan/Swift/SchoolFood.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Alamofire
+import RMPickerViewController
 
 class SchoolFoodCell:UITableViewCell {
     @IBOutlet weak var dayLabel: UILabel!
@@ -19,9 +20,10 @@ class SchoolFoodCell:UITableViewCell {
     @IBOutlet weak var emoji2Label: UILabel!
 }
 
-class SchoolFood:UITableViewController {
+class SchoolFood:UITableViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     var contentArray = [AnyObject]()
+    var week = 0
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -39,16 +41,18 @@ class SchoolFood:UITableViewController {
         self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor:UIColor.white]
         UIApplication.shared.statusBarStyle = .lightContent
         
-        self.refreshControl!.beginRefreshing()
-        self.tableView.setContentOffset(CGPoint(x: 0, y: -refreshControl!.frame.size.height), animated: true)
-        
-        reloadTableView(self.refreshControl!)
+        if self.contentArray.count == 0 {
+            self.refreshControl!.beginRefreshing()
+            self.tableView.setContentOffset(CGPoint(x: 0, y: -refreshControl!.frame.size.height), animated: true)
+            
+            reloadTableView(self.refreshControl!)
+        }
     }
 
     //UITableView Setup
     @IBAction func reloadTableView(_ sender: UIRefreshControl) {
         
-        Alamofire.request("http://skolmaten.se/berzeliusskolan/?&offset=&limit=1&fmt=json").responseJSON(completionHandler: { responseObject in
+        Alamofire.request("http://skolmaten.se/berzeliusskolan/?&offset=\(self.week)&limit=1&fmt=json").responseJSON(completionHandler: { responseObject in
             
             if let responseDict = responseObject.result.value as? NSDictionary {
                 
@@ -59,6 +63,13 @@ class SchoolFood:UITableViewController {
                 
             } else {
                 //Connection error
+                let errorController = UIAlertController.init(title: NSLocalizedString("connection-error", comment: ""), message: NSLocalizedString("check-internet", comment: ""), preferredStyle: .alert)
+                errorController.addAction(UIAlertAction.init(title: NSLocalizedString("back", comment: ""), style: .cancel, handler: nil))
+                
+                self.present(errorController, animated: true, completion: {
+                    self.tableView.reloadData()
+                    self.refreshControl!.endRefreshing()
+                })
             }
             
             self.tableView.reloadData()
@@ -103,25 +114,29 @@ class SchoolFood:UITableViewController {
         
         if contentArray.count > 0 {
             
-            let content = contentArray[indexPath.row]["items"]! as! NSArray
-            
-            for i in 0...2 {
-                switch i {
-                case 0:
-                    cell.item0Label.text = (content[i] as! String).replacingOccurrences(of: "\\(.{0,}\\)", with: "", options: .regularExpression)
-                    cell.emoji0Label.text = emojiForLine(input: (content[i] as! String))
-                    break
-                case 1:
-                    cell.item1Label.text = (content[i] as! String).replacingOccurrences(of: "\\(.{0,}\\)", with: "", options: .regularExpression)
-                    cell.emoji1Label.text = emojiForLine(input: (content[i] as! String))
-                    break
-                case 2:
-                    cell.item2Label.text = (content[i] as! String).replacingOccurrences(of: "\\(.{0,}\\)", with: "", options: .regularExpression)
-                    cell.emoji2Label.text = emojiForLine(input: (content[i] as! String))
-                    break
-                default:
-                    break
+            if let content = contentArray[indexPath.row]["items"]! as? NSArray {
+                for i in 0...2 {
+                    switch i {
+                    case 0:
+                        cell.item0Label.text = (content[i] as! String).replacingOccurrences(of: "\\(.{0,}\\)", with: "", options: .regularExpression)
+                        cell.emoji0Label.text = emojiForLine(input: (content[i] as! String))
+                        break
+                    case 1:
+                        cell.item1Label.text = (content[i] as! String).replacingOccurrences(of: "\\(.{0,}\\)", with: "", options: .regularExpression)
+                        cell.emoji1Label.text = emojiForLine(input: (content[i] as! String))
+                        break
+                    case 2:
+                        cell.item2Label.text = (content[i] as! String).replacingOccurrences(of: "\\(.{0,}\\)", with: "", options: .regularExpression)
+                        cell.emoji2Label.text = emojiForLine(input: (content[i] as! String))
+                        break
+                    default:
+                        break
+                    }
                 }
+            } else {
+                let error = UIAlertController.init(title: NSLocalizedString("error", comment: ""), message: NSLocalizedString("food-fail", comment: ""), preferredStyle: .alert)
+                error.addAction(UIAlertAction.init(title: NSLocalizedString("back", comment: ""), style: .cancel, handler: nil))
+                self.present(error, animated: true, completion: nil)
             }
         }
         
@@ -130,6 +145,8 @@ class SchoolFood:UITableViewController {
     
     func emojiForLine(input: String) -> String {
         let tagRegex = "\\(([^,)]+)"
+        
+        print(input)
         
         if input.range(of: tagRegex, options: .regularExpression, range: nil, locale: nil) != nil {
             var tag = input[input.range(of: tagRegex, options: .regularExpression, range: nil, locale: nil)!]
@@ -152,5 +169,51 @@ class SchoolFood:UITableViewController {
         } else {
             return "a"
         }
+    }
+    
+    @IBAction func changeWeek(_ sender: Any) {
+        
+        let cancelAction = RMAction<UIPickerView>(title: NSLocalizedString("cancel", comment:""), style:.cancel) { _ in
+        }
+        
+        let thisWeekAction = RMAction<UIPickerView>(title: NSLocalizedString("this-week", comment:""), style:.done) { _ in
+            self.week = 0
+            self.refreshControl!.beginRefreshing()
+            self.tableView.setContentOffset(CGPoint(x: 0, y: -self.refreshControl!.frame.size.height), animated: true)
+            self.reloadTableView(self.refreshControl!)
+        }
+        
+        let selectAction = RMAction<UIPickerView>(title: NSLocalizedString("select", comment:""), style:.done) { controller in
+            self.week = (controller.contentView.selectedRow(inComponent: 0) + 1) - (Calendar.current.component(.weekOfYear, from: Date()))
+            self.refreshControl!.beginRefreshing()
+            self.tableView.setContentOffset(CGPoint(x: 0, y: -self.refreshControl!.frame.size.height), animated: true)
+            self.reloadTableView(self.refreshControl!)
+        }
+        
+        let actionController = RMPickerViewController(style:.white, title: "", message: "", select:selectAction, andCancel:cancelAction)!
+        actionController.picker.delegate = self
+        actionController.picker.dataSource = self
+        actionController.disableBouncingEffects = true
+        actionController.disableBlurEffects = true
+        actionController.picker.selectRow(Calendar.current.component(.weekOfYear, from: Date()) + self.week - 1, inComponent: 0, animated: false)
+        
+        actionController.addAction(thisWeekAction!)
+        
+        actionController.view.tintColor = UIColor(red:0.53, green:0.08, blue:0.22, alpha:1.0)
+        
+        //Don't ask...
+        tabBarController?.present(actionController, animated: true, completion:nil)
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 52
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(row+1)
     }
 }
