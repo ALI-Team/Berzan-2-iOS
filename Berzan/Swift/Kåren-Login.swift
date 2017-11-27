c7c13a1382e77fa0439a3653e9b3a71168325467
Luka Jankovic <lukjan1999@gmail.com>
2017-11-27 22:20:49 +0100

    Init LoginVC

---
+++ Berzan/Swift/Kåren-Login.swift
@@ -0,0 +1,13 @@
+//
+//  Kåren-Login.swift
+//  Berzan
+//
+//  Created by Luka Janković on 2017-11-27.
+//  Copyright © 2017 ALI Team. All rights reserved.
+//
+
+import UIKit
+
+class loginViewControllerWrapper: ButtonBarPagerTabStripViewController {
+    
+}
